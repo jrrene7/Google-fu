56616d2f9acad150fca011ee2c6da5884e2d4511
jrrene7 <jrrene7@gmail.com>
2021-08-12 08:25:01 -0400

    search page header options

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,6 +1,8 @@
-function Avatar({url}) {
+function Avatar({url, className}) {
     return (
-        <img className="h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110"
+        <img 
+        className={`h-10 rounded-full cursor-pointer transition 
+            duration-150 transform hover:scale-110 ${className}`}
         loading="lazy"
         src={url}
         alt="profile pic"
