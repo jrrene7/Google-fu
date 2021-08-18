1c5a8642962a6bfd2b10f15f6a29743f956dcd06
jrrene7 <jrrene7@gmail.com>
2021-08-18 13:01:54 -0400

    bug fix

--- pages/search.js
+++ pages/search.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Header from "../components/Header";
-//import { API_KEY, CONTEXT_KEY } from "../keys";
+// import { API_KEY, CONTEXT_KEY } from "../keys";
 import Response from "../Response";
 import { useRouter } from "next/router";
 import SearchResults from "../components/SearchResults";
@@ -8,7 +8,7 @@ import SearchResults from "../components/SearchResults";
 function Search({results}) {
   const router = useRouter();
 
-  console.log(results);
+  
     return (
         <div>
           <Head>
@@ -31,7 +31,7 @@ export async function getServerSideProps(context) {
   // Pagination
   const startIndex = context.query.start || "0";
 
-  const data = useDummyData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`)
+  const data = useDummyData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`)
   .then((response) => response.json());
 
   //After the SERVER has rendered...  pass the results to the client
