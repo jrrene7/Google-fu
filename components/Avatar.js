de55468005de4092034e64f3426851f6230726b3
jrrene7 <jrrene7@gmail.com>
2021-08-18 12:28:22 -0400

    finishing up for deployment

--- components/Avatar.js
+++ components/Avatar.js
@@ -6,6 +6,8 @@ function Avatar({url, className}) {
         className={`h-10 rounded-full cursor-pointer transition 
             duration-150 transform hover:scale-110 ${className}`}
         loading="lazy"
+        height={50}
+        width={50}
         src={url}
         alt="profile pic"
         
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["cdn.vox-cdn.com", "www.google.co.uk"],
+    domains: ["cdn.vox-cdn.com", "www.google.co.uk", "media-exp1.licdn.com"],
   }
 }
